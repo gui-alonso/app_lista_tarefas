4541e9457f058d2ace1c46ff66ac5a21b7270d79
Guilherme <alonsogui011@gmail.com>
2023-07-31 07:17:30 -0300

    iniciando projeto app tarefas

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,54 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('APP - Lista de tarefas'),
         ),
-        body: Column()
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Tasks("Aprendendo Flutter"),
+            Tasks("Estudar ITIL"),
+            Tasks("Criar um app"),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
+
+class Tasks extends StatelessWidget {
+  final String nome;
+  const Tasks(this.nome, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.blue,
+            height: 140,
+          ),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.black26,
+                  width: 72,
+                  height: 140,
+                ),
+                Text(nome),
+                ElevatedButton(
+                    onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
